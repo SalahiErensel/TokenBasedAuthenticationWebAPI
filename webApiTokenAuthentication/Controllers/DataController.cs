a8a32dc181fc777c84cd31ab442ed365adf35489
Salahi Erensel <salahi_erensel@hotmail.com>
2023-02-15 13:04:41 +0200

    Add project files.

---
+++ webApiTokenAuthentication/Controllers/DataController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace webApiTokenAuthentication.Controllers
+{
+    public class DataController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/data/salahi")]
+        public IHttpActionResult Get()
+        {
+            return Ok("Salahi free API");
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("api/data/salahi/authenticate")]
+        public IHttpActionResult GetForAuthenticate() 
+        { 
+            var identity = (ClaimsIdentity)User.Identity;
+            return Ok(identity.Name + " is authenticated successfully");
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        [Route("api/data/salahi/authorize")]
+        public IHttpActionResult GetForAdmin()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            var roles = identity.Claims.Where(c=>c.Type == ClaimTypes.Role).Select(c=>c.Value);
+            return Ok(identity.Name + " is authenticated successfully" + " Role: " + string.Join(", ", roles.ToList()));
+        }
+    }
+}
